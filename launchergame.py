d206f0d9a8ff108cc2a8cb0562af6a6529bc8283
Sam Thomas <samdthomas08@gmail.com>
2026-03-06 20:37:14 -0500

    finsihed driving

--- launchergame.py
+++ launchergame.py
@@ -1,4 +1,8 @@
+from email.mime import text
+import random
 import pgzrun
+import time
+import math
 
 mouseX = 0
 mouseY = 0
@@ -8,23 +12,58 @@ firing = False
 yVel = 5
 WIDTH = 1000
 HEIGHT = 800
+bullets = []
+targets = []
+timer = 600
+
+class Target:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Bullet:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.yVel = 5
+        self.firing = False
 
  
 def draw():
     screen.clear()
-    screen.draw.filled_circle((bulletX,bulletY),10,"white")
+    for bullet in bullets:
+        screen.draw.filled_circle((bulletX, bulletY), 10, "white")
     screen.draw.filled_circle((mouseX,mouseY),20,"green")
-  
+    screen.draw.text(str(timer), (10, 10), color="white", fontsize=30)
+    for target in targets:
+        screen.draw.filled_circle((target.x, target.y), 20, "red")
+    if math.sqrt((bulletX - mouseX) ** 2 + (bulletY - mouseY) ** 2) < 30:
+        targets.remove(target)
+        bullets.remove(bullet)
+
+def countdown():
+    global timer
+    if timer > 0:
+        timer -= 1
+    else:
+        timer = 600
+
+def spawn_target():
+    x = random.randint(100, 900)
+    y = random.randint(100, 700)
+    target = Target(x, y)
+    targets.append(target)
 
 def update():
-    global bulletX, bulletY, firing, yVel
-    if firing == False:
-        bulletX = mouseX + 10
-        bulletY = mouseY - 10
+    global bulletX, bulletY, firing, yVel, bullets
+    for bullet in bullets:
+        bulletX += xVel
+        bulletY += yVel
     else:
         bulletX += 10
         bulletY -= yVel
         yVel -= .2
+    
         
     if bulletX >= 1020 or bulletY >= 820:
         firing = False
@@ -33,13 +72,23 @@ def update():
         yVel = 5
 
 def on_mouse_down(button):
-    global firing
+    global firing, bulletX, bulletY, yVel, xVel
     if button == mouse.LEFT:
-        if firing == False:
-            firing = True
+        bulletX = mouseX + 10
+        bulletY = mouseY - 10
+        xVel = 10
+        yVel = 5
+        bullets.append({"bulletX": bulletX, "bulletY": bulletY, "xVel": xVel, "yVel": yVel})
+
+
+
+
+        firing = True
+        bullets.append(Bullet(mouseX + 10, mouseY - 10))
 
 def on_mouse_move(pos):
   global mouseX, mouseY
   mouseX, mouseY = pos
-  
+
+spawn_target()
 pgzrun.go()
